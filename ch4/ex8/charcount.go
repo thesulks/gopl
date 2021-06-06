175b4cfbd6633a646098cb8f4c9166c4a77bf490
Dokyun Kim <cs.dkkim@gmail.com>
2021-06-06 23:16:49 +0900

    Add solution of 4.8

---
+++ ch4/ex8/charcount.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[string]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[k(r)]++
+		utflen[n]++
+	}
+	fmt.Print("category\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%s\t\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
+
+func k(r rune) string {
+	switch {
+	case unicode.IsLetter(r):
+		return "letter"
+	case unicode.IsNumber(r):
+		return "number"
+	}
+	return "other"
+}
