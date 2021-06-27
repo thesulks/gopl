a45b6cb1943eab356b47e47843ae4ed7c5646abb
Dokyun Kim <cs.dkkim@gmail.com>
2021-06-27 13:22:31 +0900

    Add solution of 7.6

---
+++ ch7/ex6/tempflag.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gopl/ch2/ex1/tempconv"
+)
+
+type celsiusFlag struct {
+	tempconv.Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = tempconv.Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = tempconv.FToC(tempconv.Fahrenheit(value))
+		return nil
+	case "K":
+		f.Celsius = tempconv.KToC(tempconv.Kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("invaild temperature %q", s)
+}
+
+func CelsiusFlag(name string, value tempconv.Celsius, usage string) *tempconv.Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, "temp", usage)
+	return &f.Celsius
+}
+
+var temp = CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
